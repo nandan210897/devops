d40bc6d9d6a0061770347a733a630f8c0061c2fc
nandan210897 <57901698+nandan210897@users.noreply.github.com>
2020-03-31 22:58:35 +0530

    Update helo.java

--- helo.java
+++ helo.java
@@ -2,13 +2,13 @@ import java.util.Scanner; // Import the Scanner class
 
 class helo {
   public static void main(String[] args) {
-    int x=115, y=100, sum;
+    int x, y, sum;
     Scanner myObj = new Scanner(System.in); // Create a Scanner object
     System.out.println("Type a number:");
-    x = myObj.nextInt(); // Read user input
+    x = 115; // Read user input
 
     System.out.println("Type another number:");
-    y = myObj.nextInt(); // Read user input
+    y = 100; // Read user input
 
     sum = x + y;  // Calculate the sum of x + y
     System.out.println("Sum is: " + sum); // Print the sum
